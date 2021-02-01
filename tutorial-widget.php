939cf76615bb4eb6698617e96100075e155ca66a
Ippei Sumida <ippey.s@gmail.com>
2021-02-01 16:07:53 +0900

    設定はファイルで持たせるように変更

--- tutorial-widget.php
+++ tutorial-widget.php
@@ -17,12 +17,8 @@ if (is_admin()) {
 	require_once(__DIR__ . '/src/class.tutorial-widget-factory.php');
 	$dashboard = new \Ippey\TutorialWidget\TutorialDashboard();
 	$factory = new \Ippey\TutorialWidget\TutorialWidgetFactory();
-	$config = [
-		'widgets' => [
-			['title' => 'ほげ１', 'filename' => 'aaa.html', 'id' => 1],
-			['title' => 'ほげ２', 'filename' => 'bbb.html', 'id' => 2],
-		],
-	];
+	$config_file_path = __DIR__ . '/config/settings.json';
+	$config = json_decode(file_get_contents($config_file_path), true);
 	foreach ($config['widgets'] as $data) {
 		$widget = $factory->create_with_data($data);
 		$dashboard->add_widget($widget);
